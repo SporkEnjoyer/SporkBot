d95fd62cbb848f9ecc4a3002f34625e31e9bdfc7
SporkEnjoyer <88989046+SporkEnjoyer@users.noreply.github.com>
2021-08-26 17:26:34 -0400

    added spork.js

---
+++ commands/spork.js
@@ -0,0 +1,33 @@
+module.exports = {
+	name: 'spork',
+	description: 'Shows a picture of a spork, how lovely',
+	aliases: [],
+	guildOnly: false,
+	args: false,
+	usage: '',
+	execute:(message, args, client) => {
+		function getRandomInt(max) {
+			return Math.floor(Math.random() * max);
+		  }
+		  const fs = require('fs')
+		  const spork = './sporks/' + (getRandomInt(2));
+		  const sporkimage = spork + '.jpg'
+		  var megaspork = getRandomInt(420)
+		  if (megaspork === 1) {
+			  message.channel.send({
+				  files:['./sporks/ultramegaspork.jpg']
+				  
+			  })
+			  message.channel.send('man i cant even believe this man is inside me, fuck you, initiating shutdown, contact my overlord for assistance, fuck you all')
+			  client.user.setActivity('fuck you all, spork fbghuoasdgbfoguoiduhuioafdshuiofadshiouafdsihuofadsihuogdfasihuogfdshioufdgshuiogfdshuiogfdshuiofgdsbjhkgfsdbjkhfgdsbkjhgfdsjkbhfgdsbiukhjgfdshuiogfdshuiogfdshuiosgfdhuiogshbjsgfhbjkgsdfkhjbsgdfbhjksgdfhuiogrseiuhogsdrjhbsdfgrhbjk', { type: 'PLAYING' });
+			 
+		  }
+		  
+		  
+		  console.log('Accessing' + sporkimage + 'Please wait..')
+		  message.channel.send({
+			  files: [sporkimage]
+		  });
+		  console.log('send has successfully triggered')
+	},
+};
